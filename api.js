b2ba51a6f464f8e7ebbdbdfd1f426fcafce7d5ca
idrodrigo <idrodrigo@icloud.com>
2023-04-28 22:41:38 -0600

    first commit

---
+++ api.js
@@ -0,0 +1,42 @@
+const apiKey = "9644790e1745234ca9cfbc086b720185";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  let errorDisplay = document.querySelector(".error").style.display;
+  let weatherDisplay = document.querySelector(".weather").style.display;
+  let emptyDisplay = document.querySelector(".empty-error").style.display;
+
+  if (response.status === 404) {
+    errorDisplay = "block";
+    weatherDisplay = "none";
+    emptyDisplay = "none";
+  }
+
+  if (response.status === 400) {
+    weatherDisplay = "none";
+    emptyDisplay = "block";
+    errorDisplay = "none";
+  }
+
+  errorDisplay = "none";
+  emptyDisplay = "none";
+
+  const data = await response.json();
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+  document.querySelector(".weather").style.display = "block";
+  let img = data.weather[0].main.toLowerCase();
+  weatherIcon.src = `images/${img}.png`;
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
---
+++ main.js
@@ -0,0 +1,37 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+<div class="card">
+<div class="search">
+    <input type="text" placeholder="enter a city name" spellcheck="false">
+    <button><img src="images/search.png" alt="search icon"></button>
+</div>
+<div class="error">
+    <p>Invalid City Name</p>
+</div>
+<div class="empty-error">
+    <p>Please enter a city name  </p>
+</div>
+<div class="weather">
+    <img src="images/rain.png" class="weather-icon" alt="raining image">
+    <h1 class="temp">22°C</h1>
+    <h2 class="city">New York</h2>
+    <div class="details">
+        <div class="col">
+            <img src="images/humidity.png" alt="humidity image">
+            <div>
+                <p class="humidity">50%</p>
+                <p>humidity</p>
+            </div>
+        </div>
+        <div class="col">
+            <img src="images/wind.png" alt="wind image">
+            <div>
+                <p class="wind">15 km/h</p>
+                <p>wind</p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+`;
